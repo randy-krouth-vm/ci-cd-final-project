9872fee060ac0c89b48357c8c6fa05e19379101d
randy-krouth-vm <randy.krouth.vm@outlook.com>
2025-08-01 06:28:24 -0400

    Fixed Linting Error

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -32,7 +32,8 @@ def bad_request(error):
     app.logger.warning(message)
     return (
         jsonify(
-            status=status.HTTP_400_BAD_REQUEST, error="Bad Request", message=message
+            status=status.HTTP_400_BAD_REQUEST, 
+            error="Bad Request", message=message
         ),
         status.HTTP_400_BAD_REQUEST,
     )

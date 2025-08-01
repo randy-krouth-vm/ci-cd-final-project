5f98e396f9290cb0e8698ac23e1ec0ee974ac089
randy-krouth-vm <randy.krouth.vm@outlook.com>
2025-08-01 06:53:05 -0400

    Fixed Linting

--- service/routes.py
+++ service/routes.py
@@ -101,7 +101,10 @@ def update_counters(name):
         name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
+        return abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Counter {name} does not exist"
+            )
 
     COUNTER[name] += 1
 
